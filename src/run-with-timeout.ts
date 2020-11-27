75a2d60c148040a0330173ce443bbecd6519fc4d
Dmytro Maretskyi <maretskii@gmail.com>
2020-11-27 17:02:11 +0100

    Fix runWithTimeout

--- src/run-with-timeout.ts
+++ src/run-with-timeout.ts
@@ -4,14 +4,16 @@
  * @param timeout
  * @param getError
  */
-export function runWithTimeout<T> (action: () => Promise<T>, timeout: number, getError?: () => Error): Promise<T> {
-  function throwOnTimeout (timeout: number, getError: () => Error): Promise<never> {
+export function runWithTimeout<T> (action: () => Promise<T>, timeout: number, error?: Error): Promise<T> {
+  // It's important to create this error object here and not inside `setTimeout` callback because this is the place where the stack trace will be captured.
+  const actualError = error ?? new Error(`Timed out in ${timeout}ms.`);
+  function throwOnTimeout (): Promise<never> {
     // eslint-disable-next-line promise/param-names
-    return new Promise((_, reject) => setTimeout(() => reject(getError()), timeout));
+    return new Promise((_, reject) => setTimeout(() => reject(actualError), timeout));
   }
 
   return Promise.race([
     action(),
-    throwOnTimeout(timeout, getError ?? (() => new Error(`Timed out in ${timeout}ms.`)))
+    throwOnTimeout()
   ]);
 }
